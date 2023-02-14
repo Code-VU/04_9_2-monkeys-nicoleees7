d3bd0e28865dc7e60acdfe698949dc639f907673
Nicole Smith <Nicole.Smith@vu.com>
2023-02-14 15:02:41 -0600

    monkey

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -1,8 +1,14 @@
 def calculateTime():
-    
     # This first line is provided for you
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
+
+    if monkey_one == "y" and monkey_two == "y":
+        print("Uh Oh! We're in trouble!")
+    elif monkey_one == "n" and monkey_two == "n":
+        print("Uh Oh! We're in trouble!")
+    else:
+        print("Yay! We're going to have a good day!")
 
     # end assignment
 
